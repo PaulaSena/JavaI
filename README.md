3c344c5acbca26ab99ebf696b8f0b7c624db62db
PaulaSena <senasilvaanapaula@gmail.com>
2023-12-19 23:29:25 -0300

    Variaveis

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Java I
+# JAVA I
 Bases do Java: os tipos, variáveis, como controlar o fluxo da aplicação com condicionais e laços, manipular textos e datas e até criar suas próprias funções em Java
 
 ### Introdução ao Java
--- src/br/com/adaletscode/java/Main.java
+++ src/br/com/adaletscode/java/Main.java
@@ -3,6 +3,48 @@ package br.com.adaletscode.java;
 public class Main {
 
     public static void main(String[] args) {
-System.out.println("Ola, mundo");
+        System.out.println("");
+        System.out.println(" ######  Variaveis Primitivas");
+        System.out.println("");
+
+        byte byteVar = 127;
+        short shortVar = 32767;
+        int intVar = 2147483647;
+        long longVar = 9223372036854775807L;
+        float floatVar = 3.14f;
+        double doubleVar = 3.14159265359;
+        char charVar = 'A';
+        boolean booleanVar = true;
+
+        // Variável de referência (String)
+        String stringVar = "Let's code";
+
+        // Imprimir variáveis
+        System.out.println("byteVar: " + byteVar);
+        System.out.println("shortVar: " + shortVar);
+        System.out.println("intVar: " + intVar);
+        System.out.println("longVar: " + longVar);
+        System.out.println("floatVar: " + floatVar);
+        System.out.println("doubleVar: " + doubleVar);
+        System.out.println("charVar: " + charVar);
+        System.out.println("booleanVar: " + booleanVar);
+        System.out.println("stringVar: " + stringVar);
+
+        System.out.println("");
+        System.out.println(" ######  Calculos Simples  ######");
+        System.out.println("");
+
+        int a;
+        int b=2;
+        a=3;
+        int soma= a+b;
+        int subtracao=a-b;
+        int multiplicao=a*b;
+        float divisao = (float) a/b; // converte int a, b para Float.
+
+        System.out.println("Soma = " +soma);
+        System.out.println("Subtracao = " +subtracao);
+        System.out.println("Multiplicacao = " +multiplicao);
+        System.out.println("Divisao = " +divisao);
     }
 }
